177a264314bc3fa67ea9a38961f4eb96952bc26b
Blake Herbert <Bwherb1@gmail.com>
2023-11-07 17:50:29 -0500

    added buttons with premise names and street names

--- script.js
+++ script.js
@@ -12,16 +12,29 @@ fetch(apiUrl)
   .then(response => response.json())
   .then(data => {
     const features = data.features;
-    const randomIndex = Math.floor(Math.random() * features.length);
-    const randomRow = features[randomIndex].attributes;
-    console.log(randomRow.premise_name);
+    const randomIndex1 = Math.floor(Math.random() * features.length);
+    const randomIndex2 = Math.floor(Math.random() * features.length);
+    const randomIndex3 = Math.floor(Math.random() * features.length);
 
-    const inspectionDate = formatDate(randomRow.InspectionDate) || "N/A";
+    const randomRow1 = features[randomIndex1].attributes;
+    const randomRow2 = features[randomIndex2].attributes;
+    const randomRow3 = features[randomIndex3].attributes;
+
+    console.log(randomRow1.premise_name);
+
+    const inspectionDate = formatDate(randomRow1.InspectionDate) || "N/A";
     
     const promptDiv = document.getElementById("prompt");
-    promptDiv.textContent = "Who got the violation: \"" + randomRow.Insp_Viol_Comments + "\" on " + inspectionDate;
+    promptDiv.textContent = "Who got the violation: \"" + randomRow1.Insp_Viol_Comments + "\" on " + inspectionDate;
 
-
+    const option1 = document.getElementById("option1");
+    option1.textContent = randomRow1.premise_name + " on " + randomRow1.premise_adr1_street;
+    const option2 = document.getElementById("option2");
+    option2.textContent = randomRow2.premise_name + " on " + randomRow2.premise_adr1_street;
+    const option3 = document.getElementById("option3");
+    option3.textContent = randomRow3.premise_name + " on " + randomRow3.premise_adr1_street;
+  
+  
   })
   .catch(error => {
     console.error("Error fetching data: " + error);
